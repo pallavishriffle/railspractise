6ec1e89cc1b5727c0acbe0c5bc25bc8da6ff6d68
Pallavi Mourya <pallavim@shriffle.com>
2023-08-10 11:22:40 +0530

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_09_124839) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_10_063352) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -24,7 +25,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_09_124839) do
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "mobile"
+    t.string "city"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
